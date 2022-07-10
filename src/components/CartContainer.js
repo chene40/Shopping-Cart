1f463aa3228183b956fe32c96099290c416e41ce
Eric <chene40@mcmaster.ca>
2022-07-10 15:52:01 -0400

    wired up CartItem component

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import CartItem from './CartItem'
-
-import { useSelector } from 'react-redux'
+import { clearCart } from '../redux/slice/cartSlice'
+import { useSelector, useDispatch } from 'react-redux'
 
 const CartContainer = () => {
+
+    const dispatch = useDispatch()
 
     const { cartItems, total, amount } = useSelector(store => store.cart)
 
@@ -33,7 +35,7 @@ const CartContainer = () => {
                         total <span> ${total} </span>
                     </h4>
                 </div>
-                <button className='btn clear-btn'> clear cart </button>
+                <button className='btn clear-btn' onClick={() => dispatch(clearCart())}> clear cart </button>
             </footer>
         </section>
     )
